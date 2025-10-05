9196ae2cbbd5466f84ebd6c1654d735dc85ddd16
Argreisto Daniel Krismiawan <argreistod@gmail.com>
2025-10-05 16:38:30 +0700

    Latihan1_3

---
+++ Semester3/Struktur Data/Pertemuan1/Lat1 Pert1_3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() 
+{	cout << "=== PROGRAM MENENTUKAN GRADE NILAI SISWA ===\n" << endl;
+	for (int i=0; i<75; i++) cout << "="; 
+	cout << endl;
+	
+    int skor;
+    	cout << "Masukkan nilai siswa: ";
+   		cin >> skor;
+		cout << endl;
+		
+    if (skor==100) 
+		{cout << "Grade: Skor sempurna!" << endl;} 
+	else if (skor>=90) 
+		{cout << "Grade: A" << endl;} 
+	else if (skor>=80) 
+		{cout << "Grade: B" << endl;} 
+	else if (skor>=70) 
+		{cout << "Grade: C" << endl;}
+	else if (skor>=60) 
+		{cout << "Grade: D" << endl;} 
+	else 
+		{cout << "Grade: F" << endl << "Belajar lagi!!!";}
+}
+
