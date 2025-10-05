65cca13341588e7848deb21dc2d62071acb63035
Argreisto Daniel Krismiawan <argreistod@gmail.com>
2025-10-05 16:36:42 +0700

    Latihan1_1

---
+++ Semester3/Struktur Data/Pertemuan1/Lat.1 Pert.1_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() 
+{	cout << "=== PROGRAM MENCETAK 3 TIPE DATA BERBEDA ===\n" << endl;
+	for (int i=0; i<75; i++) cout << "="; 
+	cout << endl;
+    
+	int angka;
+    char huruf;
+    float pecahan;
+   		cout << "Masukkan 3 nilai (integer, karakter, float)\n " << endl;
+		cout << "Input berurutan: ";
+    	cin >> angka >> huruf >> pecahan;
+    	
+    cout << "\nInteger : " <<angka<<endl;
+   	cout << "Karakter : " <<huruf<<endl;
+   	cout << "Pecahan : " <<pecahan<<endl;
+}
+
