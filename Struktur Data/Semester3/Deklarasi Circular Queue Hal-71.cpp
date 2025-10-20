8f79e187ee1086f3c00067ac4386a60ddd23dc84
Argreisto Daniel Krismiawan <argreistod@gmail.com>
2025-10-20 07:37:05 +0700

    Add files via upload

---
+++ Struktur Data/Semester3/Deklarasi Circular Queue Hal-71.cpp
@@ -0,0 +1,17 @@
+#define MAX 5
+//Deklarasi Circular Queue
+class CirQueue
+{
+	private:
+ 	int front ;
+ 	int rear ;
+ 	int count ;
+ 	int ele[MAX] ;
+ 	
+	public:
+ 	CirQueue();
+ 	int isFull();
+ 	int isEmpty();
+ 	void insertQueue(int item);
+ 	int deleteQueue(int *item);
+};
