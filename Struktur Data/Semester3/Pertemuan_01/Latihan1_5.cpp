b1b3d76576fb6bc8f7ba4b072aa9b08065660fae
Argreisto Daniel Krismiawan <argreistod@gmail.com>
2025-11-11 19:07:55 +0700

    Add files via upload

---
+++ Struktur Data/Semester3/Pertemuan_01/Latihan1_5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() 
+{	cout << "== PROGRAM MENUKAR BILANGAN A DENGAN B DAN SEBALIKNYA ==\n" << endl;
+	for (int i=0; i<75; i++) cout << "="; 
+	cout << endl;
+	
+    float A, B;
+    	cout << "Masukkan bilangan A: ";
+    	cin >> A;
+    	cout << "Masukkan bilangan B: ";
+    	cin >> B;
+
+    swap (A, B);
+    	cout << "\nSetelah ditukar:" << endl;
+    	cout << "A = " << A << endl;
+    	cout << "B = " << B << endl;
+}
+
