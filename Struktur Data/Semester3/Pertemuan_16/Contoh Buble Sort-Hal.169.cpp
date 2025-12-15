f1e958da405c3fba397e7ab530c079a65525741c
Argreisto Daniel Krismiawan <argreistod@gmail.com>
2025-12-15 13:12:10 +0700

    Add files via upload

---
+++ Struktur Data/Semester3/Pertemuan_16/Contoh Buble Sort-Hal.169.cpp
@@ -0,0 +1,44 @@
+// Program C++ untuk implementasi Bubble Sort
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fungsi swap
+void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// Fungsi untuk mengimplementasikan Bubble Sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        // Elemen terakhir sudah berada di posisi yang benar
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
+// Fungsi untuk mencetak array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+// Program utama
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+
+    cout << "Array yang diurutkan:\n";
+    printArray(arr, n);
+
+    return 0;
+}
